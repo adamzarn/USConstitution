e8c199fb9d7ccf1c2fbca5309c5544f3378fff98
Adam Zarn <adam.zarn@my.wheaton.edu>
2016-12-27 16:29:58 -0600

    Initial Commit

---
+++ USConstitution/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  USConstitution
+//
+//  Created by Adam Zarn on 12/27/16.
+//  Copyright © 2016 Adam Zarn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
